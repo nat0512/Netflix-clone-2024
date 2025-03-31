7cbd5db0a6f45757b9ecebef2d6175bcdc82ba38
nat0512 <natikokeb7@gmail.com>
2025-03-31 10:42:35 +0300

    banner

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
+import Banner from '../../components/banner/Banner'
 
 const Home = () => {
   return (
     <>
           <Header />
+
+      <Banner/>     
           <br />
-          <br />
+          <br />  
           <br />
           
       <Footer/>
